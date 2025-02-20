b16ad231d1e4803f47bda2baa127280a915e450a
Aditya Bhargava <adit@adit.io>
2025-02-20 08:39:16 -0600

    ye olde storybook button

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,21 +1,38 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-
-import { Button } from './Button';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import "../../globals.css";
+import { Button } from "@/components/ui/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
+const meta: Meta = {
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    variant: {
+      options: [
+        "default",
+        "destructive",
+        "outline",
+        "secondary",
+        "ghost",
+        "link",
+      ],
+      control: { type: "select" },
+    },
+    size: {
+      options: ["default", "sm", "lg", "icon"],
+      control: { type: "select" },
+    },
+    asChild: {
+      control: { type: "boolean" },
+    },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -25,29 +42,49 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    // label: "Button",
+    children: "Submit",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    // label: "Destructive Button",
+    variant: "destructive",
+    children: "Submit",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    // label: "Outline Button",
+    variant: "outline",
+    children: "Submit",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    // label: "Secondary Button",
+    variant: "secondary",
+    children: "Submit",
   },
 };
 
-export const Large: Story = {
+export const Ghost: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    // label: "Ghost Button",
+    variant: "ghost",
+    children: "Submit",
   },
 };
 
-export const Small: Story = {
+export const Link: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    // label: "Link Button",
+    variant: "link",
+    children: "Submit",
   },
 };
