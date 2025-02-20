495795e3d1e35d3663adf8e035fc660bd51ae729
Aditya Bhargava <adit@adit.io>
2025-02-20 10:23:57 -0600

    dark mode support

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,10 +2,16 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    darkMode: {
+      current: "dark",
+      darkClass: "dark",
+      classTarget: "html",
+      stylePreview: true,
+    },
     backgrounds: {
       values: [
+        { name: "dark", value: "#000" },
         { name: "light", value: "#fff" },
-        { name: "dark", value: "#777" },
       ],
     },
 
@@ -15,6 +21,19 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    /*   decorators: [
+      (story, context) => {
+        const bgNameDefault = "dark";
+        const bgName = context.parameters.backgrounds.default || bgNameDefault;
+        console.log({ bgName });
+        if (bgName === bgNameDefault) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+        return story();
+      },
+    ], */
   },
 };
 
--- README.md
+++ README.md
@@ -9,6 +9,9 @@
 ```bash
 pnpm add egonSchiele/ui
 ```
+
+To enable dark mode you'll need to add `class="dark"` to HTML tag. You also need to copy `globals.css` into your project in order to get all of the styling.
+
 ## Work on it
 
 ```bash
