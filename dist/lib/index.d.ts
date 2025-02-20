ab89c9e2987365ff02aaf377b3b4437669096389
Aditya Bhargava <adit@adit.io>
2025-02-20 09:45:14 -0600

    exports

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,2 +1,7 @@
 import { Button } from "./components/ui/button";
-export { Button };
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "./components/ui/table";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+export { Button, Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, Input, Label, Select, SelectContent, SelectItem, SelectTrigger, SelectValue, };
