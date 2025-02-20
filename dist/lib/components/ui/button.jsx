ecdcc1dc140c0b43cb83eba9060ba65beae2e921
Aditya Bhargava <adit@adit.io>
2025-02-20 07:53:07 -0600

    build

--- dist/lib/components/ui/button.jsx
+++ dist/lib/components/ui/button.jsx
@@ -12,7 +12,7 @@ var __rest = (this && this.__rest) || function (s, e) {
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
-import { cn } from "@/utils.js";
+import { cn } from "../../utils.js";
 const buttonVariants = cva("inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0 cursor-pointer", {
     variants: {
         variant: {
